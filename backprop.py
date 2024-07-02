b91d74abac26939c45deb6c7b2acb8a08e603f2d
midlajmarutha <midlajmarutha4@gmail.com>
2024-07-02 11:17:46 +0530

    manual backprop

--- backprop.py
+++ backprop.py
@@ -2,6 +2,7 @@ from math import *
 class Grad:
     def __init__(self,value,_children={}):
         self.value = value
+        
         self._children = set(_children)
         print(self._children)
     def __add__(self,other):
@@ -29,5 +30,21 @@ x = Grad(25)
 b = Grad(2)
 z1 = w + x
 z = z1 + b
-a = z.g()
-a.backward()
+# a = z.g()
+
+# dzdz = 1
+# dzdz1 = 
+
+print("--------------------------")
+
+# Manual back prop
+h = 0.01
+w1 = Grad(10)
+x1 = Grad(25)
+b1 = Grad(2)
+z2 = w1 + x1
+z2 = z2.value + h
+z3 = z2 + b.value
+z3 = z3
+
+print(f"dz/dz1:{(z3-z.value)/h}")
